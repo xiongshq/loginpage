39bf2206efcb1035a1357084872812dd90d57656
Fangxiang Wang <direction.wfx@gmail.com>
2016-03-22 15:23:00 -0700

    form validation, including email and empty check

--- js/main.js
+++ js/main.js
@@ -7,10 +7,24 @@ window.onload = function(){
     nav_a.onmouseout= function(){
         nav_a.classList.remove("icon--hired-dark");
         nav_a.classList.add("icon--hired");
-    }
+    };
     var submit_btn = document.getElementById("sign_in");
     submit_btn.onclick = function() {
-        
-    }
+        var email_val = document.getElementById("user_email").value;
+        var passowrd_val = document.getElementById("user_password").value;
+        if (email_val!="") {
+            var pattern = "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b";
+            pattern.test(email_val);
+        } else if (email_val="" || passowrd_val=="") {
+            document.getElementById("flashbar_error").innerHTML="Invalid email or password.";
+            document.getElementById("flashbar").style.display="block";
+        }
+        return false;
+    };
+    var close_bar = document.getElementById("flashbar_close");
+    close_bar.onclick = function(){
+        location.reload(true);
+        return false;
+    };
 
 };
