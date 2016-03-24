5b6f4d41a27f4dcdf9884c2d98af67552b47e356
xiongshq <xiongshq@gmail.com>
2016-03-23 17:16:31 -0700

    footer improved css and js modified

--- js/main.js
+++ js/main.js
@@ -12,14 +12,16 @@ window.onload = function(){
     submit_btn.onclick = function() {
         var email_val = document.getElementById("user_email").value;
         var passowrd_val = document.getElementById("user_password").value;
-        if (email_val!="") {
-            var pattern = "\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b";
-            pattern.test(email_val);
-        } else if (email_val="" || passowrd_val=="") {
+        var result=false;
+        if (email_val!=""&&passowrd_val!="") {
+            var pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            result=pattern.test(email_val);
+        } 
+        if (result==false) {
             document.getElementById("flashbar_error").innerHTML="Invalid email or password.";
-            document.getElementById("flashbar").style.display="block";
+            document.getElementById("flashbar").style.display="block";            
         }
-        return false;
+        return result;
     };
     var close_bar = document.getElementById("flashbar_close");
     close_bar.onclick = function(){
